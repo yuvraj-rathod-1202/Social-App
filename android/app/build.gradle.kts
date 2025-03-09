3bd0c0e6b34a8785975d0107762308589c641096
yuvraj-rathod-1202 <rathodyuvraj12022007>
2025-03-09 11:02:04 +0530

    initialize android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,19 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.android.application")
+    id("com.google.gms.google-services")
+}
+
+
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  // https://firebase.google.com/docs/android/setup#available-libraries
 }
 
 android {
