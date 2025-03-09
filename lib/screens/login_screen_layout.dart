211aafd8c53f49bddfc3e970f7e3223c001a76e1
yuvraj-rathod-1202 <rathodyuvraj12022007>
2025-03-09 13:06:11 +0530

    add texts and buttons in login page

--- lib/screens/login_screen_layout.dart
+++ lib/screens/login_screen_layout.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter_svg/flutter_svg.dart";
 import "package:social_app/utils/colors.dart";
+import "package:social_app/widgets/text_field_layout.dart";
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,6 +11,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailcontroller = TextEditingController();
+  final TextEditingController _passwordcontroller = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailcontroller.dispose();
+    _passwordcontroller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,16 +31,62 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Flexible(child: Container(), flex: 2),
               //image
               SvgPicture.asset(
                 'assets/ic_instagram.svg',
                 color: primaryColor,
                 height: 64,
               ),
+              const SizedBox(height: 64),
               //email
+              TextFieldInput(
+                hintText: 'Enter Your Email',
+                textInputType: TextInputType.emailAddress,
+                textEditingController: _emailcontroller,
+              ),
               //password
+              const SizedBox(height: 24),
+              TextFieldInput(
+                hintText: 'Enter Your Password',
+                textInputType: TextInputType.text,
+                textEditingController: _passwordcontroller,
+                isPass: true,
+              ),
+              const SizedBox(height: 24,),
               //login button
+              Container(
+                child: const Text('Log in'),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(4)),
+                  ),
+                  color: blueColor
+                ),
+              ),
+              const SizedBox(height: 12,),
+              Flexible(child: Container(), flex: 2,),
               //register button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: const Text("Don't have an account?"),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                    ),
+                  ),
+                  Container(
+                    child: const Text("Sign up.", style: TextStyle(fontWeight: FontWeight.bold,),),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         ),
